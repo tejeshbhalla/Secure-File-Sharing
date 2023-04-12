cca2dd6098b22856bbc55687ebb06ae5003fe05f
tejeshbhalla <62867013+tejeshbhalla@users.noreply.github.com>
2023-04-13 02:30:55 +0530

    Update settings.py

--- Varency/settings.py
+++ Varency/settings.py
@@ -162,6 +162,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/home/rsa-key-20230412/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
