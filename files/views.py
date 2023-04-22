8391a8030546566a148c035b1251c0b7bc550203
tejesh bhalla <2019536950.tejesh@ug.sharda.ac.in>
2023-04-23 01:04:23 +0530

    'added-minimal-fixe'

--- files/views.py
+++ files/views.py
@@ -556,7 +556,7 @@ class Group_Folder_Detail(APIView):
             if folder_hash=='root':
                 files=grp.files.all()
                 folders=grp.folders.all()
-                data={'files':[],'children':[],'permissions':{'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read}}
+                data={'files':[],'children':[],'parent_permissions':{'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read}}
                 for j in folders:
                     data['children'].append({'urlhash':j.urlhash,'name':j.name,'owner':j.owner.username,'is_folder':True,'path':j.order_parent(),'hash_path':j.order_parent_urlhash(),'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read,
                 'download_link':f'{settings.BACKEND_URL}api/content/folder_download/{create_media_jwt(i,get_client_ip(request))}' if per.can_download_content else None,'can_download_content':per.can_download_content})
@@ -564,7 +564,7 @@ class Group_Folder_Detail(APIView):
                     data['files'].append({'name':i.file_name,"url":f'{settings.BACKEND_URL}api/content/media/{create_media_jwt(i,get_client_ip(request))}','owner':i.owner.username,'urlhash':i.urlhash,'is_file':True,'date_created':i.date_uploaded,'size':i.filesize,'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read,'can_share':per.can_share_content,'can_download_content':per.can_download_content,
                                           'download_link':download_url_generate_sas(i,get_client_ip(request)) if per.can_download_content else None})
                 return Response(data,status=status.HTTP_200_OK)
-            data={'files':[],'children':[],'permissions':{'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read}}
+            data={'files':[],'children':[],'parent_permissions':{'can_add_delete_content':per.can_add_delete_content,'has_read':per.has_read}}
             folder=Folder.objects.get(urlhash=folder_hash)
             children=folder.children.all()
             files=folder.files.all()
