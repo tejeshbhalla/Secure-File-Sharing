a7840937a94d7da1bc3a077e4c2650262b3d46ce
tejesh bhalla <2019536950.tejesh@ug.sharda.ac.in>
2023-04-30 12:39:52 +0530

    'fixed-double-share'

--- content/extra_utils.py
+++ content/extra_utils.py
@@ -3,10 +3,23 @@ from django.core.exceptions import ValidationError
 
 
 
+def validate_double_share(obj,user):
+    name=obj.__class__.__name__
+    if 'Folder' in name:
+        folder=Internal_Share_Folders.objects.filter(folder_hash=obj,shared_with=user).first()
+        if folder:
+            raise ValidationError(f'Folder share already exists with user {user.username}')
+    if 'Files' in name:
+        file=Internal_Share.objects.filter(file_hash=obj,shared_with=user).first()
+        if file:
+            raise ValidationError(f'File share already exists with user {user.username}')
+
+
 
 def validate_share_already_exist(obj,user,owner):
     name=obj.__class__.__name__
     if 'Folder' in name:
+        validate_double_share(obj,user)
         folder=Internal_Share_Folders.objects.filter(folder_hash=obj,shared_with=user,owner=owner).first()
         if folder:
             raise ValidationError(f'Already shared folder with {user.username} kindly update their permissions in case of a change')
@@ -16,7 +29,7 @@ def validate_share_already_exist(obj,user,owner):
             raise ValidationError(f'Cant share with yourself')
     if 'Files' in name:
         file=Internal_Share.objects.filter(file_hash=obj,shared_with=user,owner=owner).first()
-        
+        validate_double_share(obj,user)
         if file:
             raise ValidationError(f'Already shared file with {user.username} kindly update their permissions in case of a change')
         if owner==user:
